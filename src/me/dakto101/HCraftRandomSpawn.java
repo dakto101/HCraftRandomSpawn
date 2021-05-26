bf2a919b65289f167dee07524f25555f138705d9
hcraft <36808098+dakto101@users.noreply.github.com>
2021-05-26 16:22:53 +0700

    1
    .

---
+++ src/me/dakto101/HCraftRandomSpawn.java
@@ -0,0 +1,16 @@
+package me.dakto101;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HCraftRandomSpawn extends JavaPlugin {
+    
+    public void onEnable() {
+    	Bukkit.getServer().getConsoleSender().sendMessage("§aHCraft Random Spawn da chay");
+    }
+    
+    public void onDisable() {
+    	
+    }
+    
+}
