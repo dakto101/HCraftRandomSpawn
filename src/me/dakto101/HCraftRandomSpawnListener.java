a82a6d08988ec133d06747eaa1eb66ba3a6648ec
hcraft <36808098+dakto101@users.noreply.github.com>
2021-05-26 22:24:04 +0700

    26/05/21 update
    Completed~!

--- src/me/dakto101/HCraftRandomSpawnListener.java
+++ src/me/dakto101/HCraftRandomSpawnListener.java
@@ -1,8 +1,10 @@
 package me.dakto101;
 
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 
 import io.netty.util.internal.ThreadLocalRandom;
@@ -17,16 +19,29 @@ public class HCraftRandomSpawnListener implements Listener {
 	@EventHandler
 	public static void onFirstJoin(final PlayerLoginEvent e) {
         if (!Bukkit.getOfflinePlayer(e.getPlayer().getUniqueId()).hasPlayedBefore()) {
-        	int centerX = 0;
-        	int centerZ = 0;
-        	int variance = 5000;
-        	
-        	int x = ThreadLocalRandom.current().nextInt(centerX - variance / 2, centerX + variance / 2);
-        	int y = 100;
-        	int z = ThreadLocalRandom.current().nextInt(centerZ - variance / 2, centerZ + variance / 2);
-            e.getPlayer().getWorld().setSpawnLocation(x, y, z);
-            Bukkit.getServer().getConsoleSender().sendMessage("§aSpawn location = " + e.getPlayer().getWorld().getSpawnLocation());
+        	setNewSpawnLocation(e.getPlayer().getWorld());
         }
+	}
+	
+    /**
+     * When players die...
+     * 
+     * @param event
+     */
+	@EventHandler
+	public static void onDeath(final PlayerDeathEvent e) {
+		setNewSpawnLocation(e.getEntity().getWorld());
+	}
+	
+	private static void setNewSpawnLocation(final World world) {
+    	int centerX = 0;
+    	int centerZ = 0;
+    	int variance = 5000;
+    	
+    	int x = ThreadLocalRandom.current().nextInt(centerX - variance / 2, centerX + variance / 2);
+    	int y = 100;
+    	int z = ThreadLocalRandom.current().nextInt(centerZ - variance / 2, centerZ + variance / 2);
+        world.setSpawnLocation(x, y, z);
 	}
 	
 }
