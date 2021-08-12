1e3534aacb8149fe0ebcccdc60afb88e34bcaccc
hcraft <36808098+dakto101@users.noreply.github.com>
2021-08-12 20:32:27 +0700

    12/08/2021 update
    Update random spawn algorithm.

--- src/me/dakto101/HCraftRandomSpawn.java
+++ src/me/dakto101/HCraftRandomSpawn.java
@@ -29,22 +29,28 @@ public class HCraftRandomSpawn extends JavaPlugin {
 	
 	private static void setNewSpawnLocationTask() {
 		BukkitScheduler s = plugin.getServer().getScheduler();
-		//Set new spawn location each 3600 seconds.
+		//Set new spawn location each 7200 seconds.
 		s.scheduleSyncRepeatingTask(plugin, () -> {
 			World w = Bukkit.getWorld("world");
+
 			if (w != null) {
-		    	int centerX = 0;
-		    	int centerZ = 0;
-		    	int variance = 5000;
-		    	
-		    	int x = ThreadLocalRandom.current().nextInt(centerX - variance / 2, centerX + variance / 2);
-		    	int y = 100;
-		    	int z = ThreadLocalRandom.current().nextInt(centerZ - variance / 2, centerZ + variance / 2);
-		        w.setSpawnLocation(x, y, z);
+				for (int i = 0; i < 50; i++) {
+			    	int centerX = 0;
+			    	int centerZ = 0;
+			    	int variance = 5000;
+			    	
+			    	int x = ThreadLocalRandom.current().nextInt(centerX - variance / 2, centerX + variance / 2);
+			    	int y = 100;
+			    	int z = ThreadLocalRandom.current().nextInt(centerZ - variance / 2, centerZ + variance / 2);
+			        if (!w.getBiome(x, y, z).toString().contains("OCEAN") && w.getTemperature(x, y, z) > 0.1) {
+			        	w.setSpawnLocation(x, y, z);
+			        	break;
+			        }
+				}
 			}
 
 			
-		}, 0L, 3600*20L);
+		}, 0L, 7200*20L);
 
 	}
     
